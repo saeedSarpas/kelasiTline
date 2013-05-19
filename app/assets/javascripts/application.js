3ecc97d95cdd1652a468b29fb98890baf902fd82
Saeed <saeed@saeed-laptop.(none)>
2013-05-20 00:36:25 +0430

    Added fade-in to background color of submit button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,16 @@ $(function() {
 		}
 	});
 });
+
+$(function() {
+	$("#submitButton").mouseover(function(e){
+		var $this = $(this);
+		$this.animate({backgroundColor: "#2e75ce"}, 100 );
+		$this.css("color", "#eee");
+	})
+	$("#submitButton").mouseout(function(e){
+		var $this = $(this);
+		$this.css("background-color", "#eee");
+		$this.css("color", "#2e75ce");
+	})
+});
