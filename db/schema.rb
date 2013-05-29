9f8e7b8eb8d6fb3dfc45ccabae5ef61f23ab524b
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2013-05-29 10:22:35 +0430

    Created User model with default values

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130527195114) do
+ActiveRecord::Schema.define(:version => 20130529045719) do
 
   create_table "posts", :force => true do |t|
     t.integer  "user_id"
@@ -24,5 +24,11 @@ ActiveRecord::Schema.define(:version => 20130527195114) do
 
   add_index "posts", ["created_at"], :name => "index_posts_on_created_at"
   add_index "posts", ["parent"], :name => "index_posts_on_parent"
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe User do
+	
+	# { 1 => "Saeed", 2 => "Amir", 3 => "Sadegh", 4 => "Hamed", 5 => "Pouria" }
+	context "Primary values" do
+
+		it "should have Saeed with the right id" do
+			User.find_by_name("Saeed").id.should == 1
+		end
+		
+		it "should have Saeed with the right id" do
+			User.find_by_name("Amir").id.should == 2
+		end
+		
+		it "should have Saeed with the right id" do
+			User.find_by_name("Sadegh").id.should == 3
+		end
+		
+		it "should have Saeed with the right id" do
+			User.find_by_name("Hamed").id.should == 4
+		end
+		
+		it "should have Saeed with the right id" do
+			User.find_by_name("Pouria").id.should == 5
+		end
+	end
+end
