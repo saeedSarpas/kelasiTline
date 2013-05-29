12211736b439ba8dea2eac00cc78d889de1f46f3
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2013-05-29 14:04:04 +0430

    Created a replies relation for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :msg, :user_id, :parent_id, :dir
   belongs_to :parent, class_name: "Post"
+  has_many :replies, class_name: "Post", foreign_key: "parent_id",
+  	dependent: :destroy
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,4 +6,10 @@ describe Post do
 		p = Post.new parent_id: Post.create.id
 		p.parent.should be_an_instance_of Post
 	end
+
+	it "Should have replies" do
+		p = Post.create
+		Post.create {|q| q.parent_id = p.id }
+		p.replies.count.should == 1
+	end
 end
