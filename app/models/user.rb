86c6d3e67f9a9a21e95dd9b750ea10ec4015c0bf
saeedSarpas <s.sarpas@gmail.com>
2013-11-13 18:58:53 +0330

    Removed GitHub model and controller and modified user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :name
+
+	def github
+		unless @github.present?
+			token = ENV["#{self.name.upcase}_GITHUB_TOKEN"]
+			@github = Octokit::Client.new access_token: token
+		end
+		@github
+	end
+
 end
